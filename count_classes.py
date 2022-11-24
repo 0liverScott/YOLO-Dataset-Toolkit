619d127242cf2d7d8516fe98eb10aed81af139ee
0liverScott <97486738+0liverScott@users.noreply.github.com>
2022-11-24 10:02:10 +0900

    Update count_classes.py

--- count_classes.py
+++ count_classes.py
@@ -1,18 +1,24 @@
 import os
 from tqdm import tqdm
+from collections import defaultdict
 
 def count():
-    classes = {f'Class {v}': 0 for v in range(num_class)}
+    classes = defaultdict(int)
+    nums = set()
     for t in tqdm(os.listdir(txt_path)):
         with open(f'{txt_path}/{t}', 'r') as txt:
             for line in txt:
-                classes[f'Class {line[0]}'] += 1
-    for k, v in classes.items():
+                classes['Class ' f'{line[0].zfill(3)}'] += 1
+                nums.add(int(line[0]))
+    for n in range(max(nums)):
+        if n not in nums:
+            classes['Class ' f'{str(n).zfill(3)}'] = 0
+    for k, v in sorted(classes.items()):
         print(f'{k}: {v}')
 
 
 if __name__ == '__main__':
     path = 'input_path'
     txt_path = f'{path}/labels'
-    num_class = 15  # Dataset Number of Classes
+
     count()
